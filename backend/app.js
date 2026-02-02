cbd04b0045943e374f74f1a14649d6be9b92eeb3
AnshUlKumar-code <anshulkumar73610@gmail.com>
2026-02-02 14:17:12 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -30,8 +30,13 @@ app.use('/api/user',userRouter);
 app.use("/api/product",productRouter)
 app.use("/api/cart",cartRouter)
 app.use('/api/order',orderRouter)
+app.use((req, res) => {
+  res.status(404).send("Route not found");
+});
 
-
+app.get("/", (req, res) => {
+  res.send("E-Commerce Backend is running!");
+})
 
 
 app.listen(port,()=>{
